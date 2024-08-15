de3ea80843d2f861fa125cb3f19670787fce8608
Cpapot <corentinpapot2004@gmail.com>
2024-08-15 17:38:10 +0200

    add /0

--- srcs/ft_strcpy.s
+++ srcs/ft_strcpy.s
@@ -8,10 +8,10 @@ ft_strcpy: ; rdi (1er param), rsi (2eme param)
 	WHILESTR:
 		mov al, BYTE[rsi+rcx*1]		; on met le char a l'index rcx dans al
 
+		mov BYTE[rdi+rcx*1], al		; on deplace le char de la src dans la dest
+
 		cmp al, 0					; on compare al avec 0
 		je END_WHILESTR				; JAE (Jump if equal)
-
-		mov BYTE[rdi+rcx*1], al		; on deplace le char de la src dans la dest
 
 		inc rcx						; on incrémente rcx
 		jmp WHILESTR				; on jump au debut de la boucle
